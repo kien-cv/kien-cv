72eabe45cb40ca00258e99d3f3f3d03e08901a24
Ngo Trung Kien <kienhp1hp@gmail.com>
2020-11-03 23:45:11 +0700

    U

---
+++ javascript.js
@@ -0,0 +1,47 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+function inc_fz(){
+    fz = document.getElementsByClassName("resume");
+    for(var i=0;i<fz.length;i++){
+        fz[i].style.fontSize = "150%" ;
+    }
+	
+}
+
+function inc_fz1(){
+    fz = document.getElementsByClassName("resume");
+    for(var i=0;i<fz.length;i++){
+        fz[i].style.fontSize = "100%" ;
+    }
+	
+}
+
+function change_color(){
+	fz = document.getElementsByClassName("resume");
+	for(var i=0;i<fz.length;i++){
+		fz[i].style.color = "red" ;
+	}
+}
+
+function normal_color(){
+	fz = document.getElementsByClassName("resume");
+	for(var i=0;i<fz.length;i++){
+		fz[i].style.color = "black" ;
+	}
+}
+
+
+
